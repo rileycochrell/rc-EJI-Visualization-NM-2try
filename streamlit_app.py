1a421cbd03fa41c441e650935996baafb87c7ccb
rileycochrell <rileycochrell@gmail.com>
2025-11-13 14:39:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ st.set_page_config(
     layout="wide"
 )
 with st.sidebar:
-    st.page_link('streamlit_app.py', label='🌎 EJI Visualization', icon='🌎')
-    st.page_link('pages/1_What_Goes_Into_EJI.py', label='🧩 What Goes Into the EJI?', icon='🧩')
-    st.page_link('pages/2_EJI_Scale_and_Categories.py', label='🌡️ Understanding the EJI Scale', icon='🌡️')
+    st.page_link('streamlit_app.py', label='EJI Visualization', icon='🌎')
+    st.page_link('pages/1_What_Goes_Into_EJI.py', label='What Goes Into the EJI?', icon='🧩')
+    st.page_link('pages/2_EJI_Scale_and_Categories.py', label='Understanding the EJI Scale', icon='🌡️')
 
 
 # ------------------------------
