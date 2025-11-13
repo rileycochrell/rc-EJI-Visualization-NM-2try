e2ac8742279ec8be592a3f5aa1d84970879fea47
rileycochrell <rileycochrell@gmail.com>
2025-11-13 14:50:20 -0700

    Update 2_EJI_Scale_and_Categories.py

--- pages/2_EJI_Scale_and_Categories.py
+++ pages/2_EJI_Scale_and_Categories.py
@@ -3,6 +3,11 @@ import pandas as pd
 import plotly.graph_objects as go
 
 st.set_page_config(page_title="EJI Scale and Categories", layout="wide")
+st.markdown('<style>div[data-testid="stSidebarNav"] {display: none;}</style>', unsafe_allow_html=True)
+with st.sidebar:
+    st.page_link('streamlit_app.py', label='EJI Visualization', icon='🌎')
+    st.page_link('pages/1_What_Goes_Into_EJI.py', label='What Goes Into the EJI?', icon='🧩')
+    st.page_link('pages/2_EJI_Scale_and_Categories.py', label='Understanding the EJI Scale', icon='🌡️')
 
 st.title("🌡️ Understanding the EJI Scale")
 
