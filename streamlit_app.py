18d59e8276ab6fba2eb9ac656b5e66da86f5d64a
rileycochrell <rileycochrell@gmail.com>
2025-11-13 18:12:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ div[data-testid="stLogoSpacer"]::before {
     display: flex;
     align-items: center;
     padding-left: 10px; /* Add some padding for alignment */
-    padding-top: 10px;
+    padding-top: 20px;
     height: 100%;
     /* Ensure no logo image is displayed if it was implicitly added */
     background-image: none !important;
