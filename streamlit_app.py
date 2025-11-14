a719c84ac9f438844d322839c143260a3120a69f
rileycochrell <rileycochrell@gmail.com>
2025-11-13 18:42:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.markdown(
 /* Target the logo spacer div and inject text using the ::before pseudo-element */
 div[data-testid="stLogoSpacer"]::before {
     content: "🌎 Environmental Justice in New Mexico";
-    font-size: 15px; /* Adjust font size as needed */
+    font-size: 21px; /* Adjust font size as needed */
     font-weight: bold;
     color: inherit; /* Use the default sidebar text color */
     display: flex;
